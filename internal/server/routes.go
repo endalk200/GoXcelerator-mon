c6b24a44ecab553974244db2561a7d192cf19d77
endalk200 <eb808826@gmail.com>
2024-09-09 13:53:05 +0300

    feat: adds database models and functions generated by sqlc
    - adds basic signup endpoint that implements rudimentary functinality for signup

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,34 +5,73 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
+
+	"github.com/endalk200/GoXcelerator/internal/database"
+	"github.com/endalk200/GoXcelerator/internal/utils"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+var validate *validator.Validate
+
 func (s *Server) RegisterRoutes() http.Handler {
+	validate = validator.New()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	r.Get("/", s.HelloWorldHandler)
 
-	r.Get("/health", s.healthHandler)
+	r.Post("/auth/signup", s.SignupHandler)
 
 	return r
 }
 
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	type Response struct {
+		Message string `json:"message"`
 	}
 
-	_, _ = w.Write(jsonResp)
+	utils.Response(w, http.StatusOK, Response{
+		Message: "Hello world",
+	})
 }
 
-func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResp, _ := json.Marshal(s.db.Health())
-	_, _ = w.Write(jsonResp)
+type CreateUserRequestSchema struct {
+	FirstName string `validate:"required"`
+	LastName  string `validate:"required"`
+	Email     string `validate:"required,email"`
+	Password  string `validate:"required"`
+}
+
+func (s *Server) SignupHandler(w http.ResponseWriter, r *http.Request) {
+	var body CreateUserRequestSchema
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		utils.ResponseError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	if err := validate.Struct(body); err != nil {
+		// Extract validation errors
+		validationErrors := err.(validator.ValidationErrors)
+		utils.ResponseError(w, http.StatusBadRequest, validationErrors.Error())
+		return
+	}
+
+	ctx := r.Context()
+	user, err := s.db.AddUser(ctx, database.AddUserParams{
+		FirstName: body.FirstName,
+		LastName:  body.LastName,
+		Email:     body.Email,
+		Password:  body.Password,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+		utils.ResponseError(w, http.StatusInternalServerError, "Failed to create a new user")
+		return
+	}
+
+	utils.Response(w, http.StatusCreated, user)
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/endalk200/GoXcelerator/internal/database"
@@ -15,15 +18,24 @@ import (
 type Server struct {
 	port int
 
-	db database.Service
+	db *database.Queries
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
+
+	dbConnectionString := "postgres://admin:admin@localhost:5432/my-db?sslmode=disable"
+
+	connection, err := pgxpool.New(context.Background(), dbConnectionString)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v", err)
+	}
+
+	queries := database.New(connection)
 	NewServer := &Server{
 		port: port,
 
-		db: database.New(),
+		db: queries,
 	}
 
 	// Declare Server config
